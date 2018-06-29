d85fb9c4b353b91922619ab669920450868f5e35
jimroth <jimroth@users.noreply.github.com>
2018-06-29 10:21:19 -0700

    Fixed es-mapping JSON and added ingest script

--- api/ingest.sh
+++ api/ingest.sh
@@ -1,3 +1,33 @@
+#! /bin/bash
+
+HOST=localhost:9200
+INDEX=$1
+BUCKET=$2
+
+if [ $# -ne 2 ]; then
+    echo "Wrong number of arguments"
+    echo "Usage: $0 index bucket"
+    exit 1
+fi
+
+echo Build index $INDEX from bucket $BUCKET
+
+echo Delete existing index
+curl -XDELETE $HOST/$INDEX; echo
+
+echo Create new index
+curl -XPUT -H "Content-Type: application/json" $HOST/$INDEX --data-binary '
+{
+    "settings": {
+        "index": {
+            "number_of_shards": 5,
+            "number_of_replicas": 2
+        }
+    }
+}'; echo
+
+echo Add mapping for doc
+curl -XPUT -H "Content-Type: application/json" $HOST/$INDEX/_mapping/doc --data-binary '
 {
     "properties": {
         "hour": {
@@ -108,4 +138,13 @@
             }
         }
     }
-}
+}'; echo
+
+CONF="input { stdin { codec => json_lines } } output { elasticsearch { hosts => [\"$HOST\"] index => \"$INDEX\" } }"
+echo "$CONF"
+
+aws s3 cp s3://$BUCKET/$INDEX.json.gz .
+
+gunzip -c $INDEX.json.gz | logstash -e "$CONF"
+
+exit 0
