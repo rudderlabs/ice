4c962aff5af2b68c8886511703085a5fd1c83d82
jimroth <jimroth@users.noreply.github.com>
2019-03-04 12:24:14 -0800

    Clean up kubernetes tagger config file format.

--- src/java/com/netflix/ice/processor/kubernetes/Tagger.java
+++ src/java/com/netflix/ice/processor/kubernetes/Tagger.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,14 @@ import org.slf4j.LoggerFactory;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.google.gson.JsonDeserializer;
 import com.netflix.ice.common.AwsUtils;
 import com.netflix.ice.common.ResourceService;
 import com.netflix.ice.processor.kubernetes.KubernetesReport.KubernetesColumn;
@@ -28,7 +37,7 @@ public class Tagger {
 	
 	private final ResourceService resourceService;
 	private final int numCustomTags;
-	private final TaggerConfig config;
+	protected final TaggerConfig config;
 	
 	class TaggerConfig {
 		private String[] tagsToCopy;
@@ -39,20 +48,19 @@ public class Tagger {
 			rules = Lists.newArrayList();
 			for (String nameValuePair: rulesFromProperties.keySet()) {
 				String[] parts = nameValuePair.split("\\.");
-				int tagIndex = resourceService.getUserTagIndex(parts[0]);
-				this.rules.add(new Rule(tagIndex, parts[1], rulesFromProperties.get(nameValuePair)));
+				this.rules.add(new Rule(parts[0], parts[1], rulesFromProperties.get(nameValuePair)));
 			}			
 		}
 		
 		TaggerConfig(String json) {
-			Gson gson = new Gson();
+			Gson gson = new GsonBuilder().registerTypeAdapter(Rule.class, new JsonRuleDeserializer()).create();
 			TaggerConfig c = gson.fromJson(json, this.getClass());
 			this.rules = c.rules;
 			this.tagsToCopy = c.tagsToCopy;
 		}
 		
 		public String toJSON() {
-			Gson gson = new Gson();
+			Gson gson = new GsonBuilder().registerTypeAdapter(Rule.class, new JsonRuleSerializer()).create();
 	    	return gson.toJson(this);
 		}
 		
@@ -64,7 +72,7 @@ public class Tagger {
 			return rules;
 		}
 	}
-
+	
 	public Tagger(String[] tagsToCopy, Map<String, String> rules, ResourceService resourceService, String workBucketName, String workBucketPrefix, String localDir) throws IOException {
 		this.resourceService = resourceService;
 		this.numCustomTags = resourceService.getCustomTags().length;
@@ -147,29 +155,39 @@ public class Tagger {
 	}
 	
 	class Rule {
-		private final int tagIndex;
-		private final UserTag value;
-		private final List<Pattern> patterns;
+		private final String tagName;		
+		private final UserTag value;		
+		private final String patterns;
 		
-		Rule(int tagIndex, String value, String patterns) {
-			this.tagIndex = tagIndex;
+		private final int tagIndex;
+		private final List<Pattern> compiledPatterns;
+
+		
+		Rule(String tagName, String value, String patterns) {
+			this.tagName = tagName;
+			this.tagIndex = resourceService.getUserTagIndex(tagName);
 			this.value = UserTag.get(value);
+			this.patterns = patterns;
 			String[] patternStrings = patterns.split(",");
-			this.patterns = Lists.newArrayList();
+			this.compiledPatterns = Lists.newArrayList();
 			for (String p: patternStrings) {
 				if (p.isEmpty())
 					continue;
-				this.patterns.add(Pattern.compile(p));
+				this.compiledPatterns.add(Pattern.compile(p));
 			}
 		}
 		
 		public boolean matches(String namespace) {
-			for (Pattern p: patterns) {
+			for (Pattern p: compiledPatterns) {
 				Matcher m = p.matcher(namespace);
 				if (m.matches())
 					return true;
 			}
 			return false;
+		}
+		
+		public String getTagName() {
+			return tagName;
 		}
 		
 		public int getTagIndex() {
@@ -179,5 +197,38 @@ public class Tagger {
 		public UserTag getValue() {
 			return value;
 		}
+		
+		public String getPatterns() {
+			return patterns;
+		}
+	}
+	
+	class JsonRuleSerializer implements JsonSerializer<Rule> {
+
+		@Override
+		public JsonElement serialize(Rule rule, Type typeOfT,
+				JsonSerializationContext context) {
+			JsonObject json = new JsonObject();
+			json.addProperty("tagName", rule.tagName);
+			json.addProperty("value", rule.value.name);
+			json.addProperty("patterns", rule.patterns);
+			return json;
+		}
+		
+	}
+	
+	class JsonRuleDeserializer implements JsonDeserializer<Rule> {
+
+		@Override
+		public Rule deserialize(JsonElement json, Type typeOfT,
+				JsonDeserializationContext context) throws JsonParseException {
+			JsonObject jsonObject = json.getAsJsonObject();
+			Rule rule = new Rule(
+					jsonObject.get("tagName").getAsString(),
+					jsonObject.get("value").getAsString(),
+					jsonObject.get("patterns").getAsString());
+			return rule;
+		}
+		
 	}
 }
--- src/test/com/netflix/ice/processor/kubernetes/TaggerTest.java
+++ src/test/com/netflix/ice/processor/kubernetes/TaggerTest.java
@@ -2,11 +2,13 @@ package com.netflix.ice.processor.kubernetes;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.collect.Maps;
@@ -14,9 +16,13 @@ import com.netflix.ice.basic.BasicProductService;
 import com.netflix.ice.basic.BasicResourceService;
 import com.netflix.ice.common.ProductService;
 import com.netflix.ice.common.ResourceService;
+import com.netflix.ice.processor.kubernetes.Tagger.TaggerConfig;
 import com.netflix.ice.tag.UserTag;
 
 public class TaggerTest {
+	private ResourceService rs;
+	private ProductService ps;
+	private String[] customTags = new String[]{"Tag1", "Tag2", "Tag3"};
 	
 	class TestKubernetesReport extends KubernetesReport {
 
@@ -38,15 +44,18 @@ public class TaggerTest {
 			return item[2];
 		}
 	}
+	
+	@Before
+	public void init() {
+		ps = new BasicProductService(null);
+		Map<String, List<String>> tagKeys = Maps.newHashMap();
+		Map<String, List<String>> tagValues = Maps.newHashMap();
+		rs = new BasicResourceService(ps, customTags, new String[]{}, tagKeys, tagValues, null);
+	}
 
 
 	@Test
-	public void testTagger() {
-		ProductService ps = new BasicProductService(null);
-		Map<String, List<String>> tagKeys = Maps.newHashMap();
-		Map<String, List<String>> tagValues = Maps.newHashMap();
-		String[] customTags = new String[]{"Tag1", "Tag2", "Tag3"};
-		ResourceService rs = new BasicResourceService(ps, customTags, new String[]{}, tagKeys, tagValues, null);
+	public void testTagger() throws IOException {
 		TestKubernetesReport tkr = new TestKubernetesReport(new DateTime("2019-01", DateTimeZone.UTC), customTags);
 		String[] item = new String[]{ "dev-usw2a", "bar", "foobar" };
 		
@@ -54,7 +63,7 @@ public class TaggerTest {
 		Map<String, String> rules = Maps.newHashMap();
 		rules.put("Tag2.Foo", "bar");
 		rules.put("Tag1.Bar", ".*bar.*");
-		Tagger t = new Tagger(tagsToCopy, rules, rs);
+		Tagger t = new Tagger(tagsToCopy, rules, rs, null, null, null);
 		UserTag[] userTags = new UserTag[customTags.length];
 		t.tag(tkr, item, userTags);
 		assertEquals("Incorrect tagged value", "Bar", userTags[0].name);
@@ -74,5 +83,27 @@ public class TaggerTest {
 		userTags[2] = UserTag.get("overwriteMe");
 		t.tag(tkr, item, userTags);
 		assertEquals("Tag3 was not overwritten", "useMe", userTags[2].name);
+	}
+	
+	@Test
+	public void testTaggerConfig() throws IOException {
+		String[] tagsToCopy = new String[]{ "Tag3" };
+		Map<String, String> rules = Maps.newHashMap();
+		rules.put("Tag2.Foo", "bar");
+		rules.put("Tag1.Bar", ".*bar.*");
+		
+		Tagger t = new Tagger(tagsToCopy, rules, rs, null, null, null);
+		
+		String json = t.config.toJSON();		
+		TaggerConfig testConfig = t.new TaggerConfig(json);
+		assertEquals("tagsToCopy doesn't match", String.join(",", t.config.getTagsToCopy()), String.join(",", testConfig.getTagsToCopy()));
+		for (int i = 0; i < t.config.getRules().size(); i++) {
+			Tagger.Rule orig = t.config.getRules().get(i);
+			Tagger.Rule test = testConfig.getRules().get(i);
+			assertEquals("tagName doesn't match", orig.getTagName(), test.getTagName());
+			assertEquals("tagIndex doesn't match", orig.getTagIndex(), test.getTagIndex());
+			assertEquals("value doesn't match", orig.getValue(), test.getValue());
+			assertEquals("patterns doesn't match", orig.getPatterns(), test.getPatterns());			
+		}
 	}
 }
