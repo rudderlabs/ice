20d93401f707262e7c12dc446f66e4b0160c56d9
jimroth <jimroth@users.noreply.github.com>
2020-02-03 15:02:09 -0800

    Fix import for StringUtils

--- src/java/com/netflix/ice/processor/pricelist/InstancePrices.java
+++ src/java/com/netflix/ice/processor/pricelist/InstancePrices.java
@@ -24,7 +24,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.slf4j.Logger;
