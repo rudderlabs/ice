acbfe5b66e52c6f679c8add602fc8ddf4e5e7d3a
jimroth <jimroth@users.noreply.github.com>
2020-03-31 11:24:12 -0700

    Removed tests for deleted method getPattern()

--- src/java/com/netflix/ice/processor/CostAndUsageReportProcessor.java
+++ src/java/com/netflix/ice/processor/CostAndUsageReportProcessor.java
@@ -28,8 +28,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.compress.archivers.ArchiveEntry;
