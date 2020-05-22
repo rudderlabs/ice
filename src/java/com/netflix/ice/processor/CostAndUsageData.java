03842f5e84b11eef3fe3cdd4afc0432634c1f639
jimroth <jimroth@users.noreply.github.com>
2020-05-22 13:11:31 -0700

    Shutdown archive thread pool when done writing data.

--- src/java/com/netflix/ice/processor/CostAndUsageData.java
+++ src/java/com/netflix/ice/processor/CostAndUsageData.java
@@ -25,6 +25,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -287,7 +288,27 @@ public class CostAndUsageData {
 				logger.error("Error archiving file: " + s);
 			}
 		}
+		
+		shutdownAndAwaitTermination(pool);
     }
+    
+    private void shutdownAndAwaitTermination(ExecutorService pool) {
+    	pool.shutdown(); // Disable new tasks from being submitted
+    	try {
+    		// Wait a while for existing tasks to terminate
+    		if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+    			pool.shutdownNow(); // Cancel currently executing tasks
+    			// Wait a while for tasks to respond to being cancelled
+    			if (!pool.awaitTermination(60, TimeUnit.SECONDS))
+    				System.err.println("Pool did not terminate");
+    		}
+    	} catch (InterruptedException ie) {
+    		// (Re-)Cancel if current thread also interrupted
+    		pool.shutdownNow();
+    		// Preserve interrupt status
+    		Thread.currentThread().interrupt();
+    	}
+	}
     
     private void verifyTagGroups() throws Exception {    	
         for (Product product: costDataByProduct.keySet()) {
